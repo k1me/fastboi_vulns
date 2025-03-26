e13a1f380bd8546d45fd5d00a63f0f3c3f5133cf
k1me <m.kiraly21@proton.me>
2025-03-26 16:32:21 -0400

    file deletion dir-traversal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse, HTMLResponse
 import os
 
 app = FastAPI()
@@ -7,7 +7,7 @@ app = FastAPI()
 # ez a dir ahonnan ki szeretnenk olvasni a file tartalmat
 BASE_DIR = "static_files"
 
-# endpoint
+#---------------read endpoint---------------
 @app.get("/read-file/")
 async def read_file(filename: str):
     file_path = os.path.join(BASE_DIR, filename)
@@ -16,4 +16,18 @@ async def read_file(filename: str):
         with open(file_path, "r") as f:
             return PlainTextResponse(f.read())
     except FileNotFoundError:
-        return {"error": "File not found"}
+        return {"error ": "File not found"}
+        
+
+#---------------delete endpoint---------------
+@app.delete("/delete-file/")
+async def delete_file(filename: str):
+	file_path = os.path.join(BASE_DIR, filename)
+	
+	try:
+		os.remove(file_path)
+		return {"status ": "success ", " message": f"Deleted {filename}"}
+	except FileNotFoundError:
+		return FileNotFoundError
+	except Exception as e:
+		return {"error ":str(e)}
